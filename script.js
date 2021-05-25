e6010eed7005de615bba4c71f48cfea9f9030f68
AfroBadger <mark.seymour@gmail.com>
2021-05-25 09:58:48 +0100

    Working Quote-Generator

--- script.js
+++ script.js
@@ -1,24 +1,75 @@
-// let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+// Show Loading wheel
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 
 // Show New Quote
 function newQuote(){
+    // Loading wheel
+    loading();
+    //Picks from local quotes in quotes.js
+    //const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    
     // Pick a Random Quote from apiQuotes array
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if author field is bland and replace with unknown
+    if (!quote.author){
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check Quote Length to determin stylig
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    // Set quote & Hide loader
+    quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes from API
-// async function getQuotes() {
-//     const apiUrl = 'https://type.fit/api/quotes';
-//     try {
-//         const responce = await fetch(apiUrl);
-//         apiQuotes = await responce.json();
-//         newQuote();
-//     } catch (error) {
-//         // Catch Error Here
-//     }
-// }
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const responce = await fetch(apiUrl);
+        apiQuotes = await responce.json();
+        newQuote();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// To Tweet a Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
-// getQuotes();
-newQuote();
+getQuotes();
+
+//newQuote();
