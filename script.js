499257bc5c32785b2cb4daf84b75a268adbca4b8
AfroBadger <mark.seymour@gmail.com>
2021-05-26 17:42:46 +0100

    added api failover to local file and status indicator

--- script.js
+++ script.js
@@ -4,31 +4,55 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
+const apiStatus = document.getElementById('api-status')
 
 let apiQuotes = [];
+let errorCount = 0;
+let quoteSourceApi = true;
 
 // Show Loading wheel
-function loading() {
+function showLoadingWheel() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
 // Hide Loading
-function complete() {
+function hideLoadingWheel() {
     loader.hidden = true;
     quoteContainer.hidden = false;
 }
 
+function apiStatusDown() {
+    const apiUpIcon = document.createElement("i");
+    apiUpIcon.className = 'fas fa-exclamation-triangle';
+    apiStatus.appendChild(apiUpIcon);
+
+}
+
+function apiStatusUp() {
+    const apiUpIcon = document.createElement("i");
+    apiUpIcon.className = 'fas fa-signal';
+    apiStatus.appendChild(apiUpIcon);
+}
 
 // Show New Quote
 function newQuote(){
     // Loading wheel
-    loading();
+    showLoadingWheel();
     //Picks from local quotes in quotes.js
     //const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    
+    let quote = ""
+    if (quoteSourceApi) {
     // Pick a Random Quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+       quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+       console.log('source from API')
+       apiStatusUp();
+    }
+    else {
+        quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+        console.log('source from local file')
+        apiStatusDown();
+    }
     // Check if author field is bland and replace with unknown
     if (!quote.author){
         authorText.textContent = 'Unknown';
@@ -43,12 +67,12 @@ function newQuote(){
     }
     // Set quote & Hide loader
     quoteText.textContent = quote.text;
-    complete();
+    hideLoadingWheel();
 }
 
 // Get Quotes from API
 async function getQuotes() {
-    loading();
+    showLoadingWheel();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const responce = await fetch(apiUrl);
@@ -56,8 +80,18 @@ async function getQuotes() {
         newQuote();
     } catch (error) {
         // Catch Error Here
+        console.log('unable to load from api')
+        errorCount++;
+        if (errorCount < 3) {
+            getQuotes();
+        }
+        else {
+            quoteSourceApi = false;
+            newQuote();
+        }
     }
 }
+
 
 // To Tweet a Quote
 function tweetQuote() {
